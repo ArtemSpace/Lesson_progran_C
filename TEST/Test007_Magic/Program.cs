3b2b998f047a6d397192a3512f815e3232363dfb
ArtemSpace <115339889+ArtemSpace@users.noreply.github.com>
2022-10-17 01:59:51 +0700

    правка1

--- TEST/Test007_Magic/Program.cs
+++ TEST/Test007_Magic/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Clear();
+Console.Clear();
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
 
@@ -6,12 +6,12 @@ int xa =1, ya =1,
     xb =1, yb =30,
     xc =80, yc =30;
 
- Console.SetCursorPosition(xa, ya);
- Console.WriteLine("+");
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
 
- Console.SetCursorPosition(xb, yb);
- Console.WriteLine("+"); 
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+"); 
 
- Console.SetCursorPosition(xc, yc);
- Console.WriteLine("+");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
  
