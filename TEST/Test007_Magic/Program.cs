0cff5f664e0ded9d03e4cd05d7b03154f0ea7db5
Artem <art-nov@list.ru>
2022-10-17 02:05:42 +0700

    неопознанная проблема

--- TEST/Test007_Magic/Program.cs
+++ TEST/Test007_Magic/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.Clear();
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
-
-int xa =1, ya =1,
-    xb =1, yb =30,
-    xc =80, yc =30;
-
+  
+ int xa =1, ya =1,
+     xb =1, yb =30,
+     xc =80, yc =30;
+ 
  Console.SetCursorPosition(xa, ya);
  Console.WriteLine("+");
 
@@ -14,4 +14,3 @@ int xa =1, ya =1,
 
  Console.SetCursorPosition(xc, yc);
  Console.WriteLine("+");
- 
