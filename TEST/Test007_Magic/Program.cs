ff8a5eea36b9cfc97d7614d4fd9c8434b4ee1264
Artem <art-nov@list.ru>
2022-10-18 02:59:38 +0700

    решили конфликт

--- TEST/Test007_Magic/Program.cs
+++ TEST/Test007_Magic/Program.cs
@@ -1,7 +1,6 @@
-Console.Clear();
+﻿Console.Clear();
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
-<<<<<<< HEAD
   
  int xa =1, ya =1,
      xb =1, yb =30,
@@ -9,24 +8,10 @@ Console.Clear();
  
  Console.SetCursorPosition(xa, ya);
  Console.WriteLine("+");
-=======
-
-int xa =1, ya =1,
-    xb =1, yb =30,
-    xc =80, yc =30;
-
-Console.SetCursorPosition(xa, ya);
-Console.WriteLine("+");
->>>>>>> 3b2b998f047a6d397192a3512f815e3232363dfb
 
 Console.SetCursorPosition(xb, yb);
 Console.WriteLine("+"); 
 
-<<<<<<< HEAD
- Console.SetCursorPosition(xc, yc);
- Console.WriteLine("+");
-=======
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
  
->>>>>>> 3b2b998f047a6d397192a3512f815e3232363dfb
