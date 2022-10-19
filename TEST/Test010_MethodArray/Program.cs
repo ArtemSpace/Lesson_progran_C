1ac23424648ceb58b68b2b484d378e1a218f74ca
Artem <art-nov@list.ru>
2022-10-20 01:01:55 +0700

    Test010_MethodArray

---
+++ TEST/Test010_MethodArray/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] array = {1,12,31,4,6,43,77,55,75,43};
+
+int n = array.Length;
+int find = 43;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    
+    
+    //index = index + 1;
+    index++;
+}
+
